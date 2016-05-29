707bd3de062b9977d4078986bd16b3feb08970c4
Joshua Gigg <giggsey@gmail.com>
2016-05-29 22:57:01 +0100

    Add getDisplayRegion() and data builder
    This uses data from unicode-cldr/cldr-localenames-full to generate PHP
    arrays containing the data.
    
    The unit tests are currently testing every combination of locale, so
    it's currently executing around 450,000 tests. The data generated is
    from CLDR 27.0.1, which is the version installed on my Ubuntu 16.04
    machine. Once the code has been tidied up, the unit tests will be
    reduced, and the mechanism will be tested instead of the raw data.
    
    ToDos:
    
     - Any @todo's marked in the code
     - Use phing to generate the metadata (similar to
       giggsey/libphonenumber-for-php)
     - Add unit tests to generate data, and run tests on that
     - Create a special unit test to run the phing tasks, and check that
       there are no differences between the committed version, and the
       generated version.

--- src/Locale.php
+++ src/Locale.php
@@ -5,6 +5,8 @@ namespace Giggsey\Locale;
 
 class Locale
 {
+    protected static $dataDir = '../data/';
+
     /**
      * Gets the primary language for the input locale
      *
@@ -43,5 +45,52 @@ class Locale
         }
 
         return $region;
+    }
+
+    public static function getDisplayRegion($locale, $in_locale)
+    {
+        $dataDir = __DIR__ . DIRECTORY_SEPARATOR . static::$dataDir;
+
+        // Convert $locale into a region
+        $region = static::getRegion($locale);
+
+        $regionList = require $dataDir . '_list.php';
+
+        /*
+         * Loop through each part of the $in_locale, and see if we have data for that locale
+         *
+         * E.g zh-Hans-HK will look for zh-Hanks-HK, zh-Hanks, then finally zh
+         */
+        $fallbackParts = explode('-', str_replace('_', '-', $in_locale));
+        $fileToSearch = null;
+
+        $i = count($fallbackParts);
+        while ($i > 0) {
+            $searchLocale = implode('-', $fallbackParts);
+
+            if (isset($regionList[$searchLocale])) {
+                $fileToSearch = $searchLocale;
+                break;
+            }
+
+            array_pop($fallbackParts);
+            $i--;
+        }
+
+        /*
+         * Load data file, and load the region (if it exists) from it
+         */
+
+        if ($fileToSearch !== null) {
+            // Load data file
+
+            $data = require $dataDir . $fileToSearch . '.php';
+
+            if (isset($data[$region])) {
+                return $data[$region];
+            }
+        }
+
+        return '';
     }
 }
--- tests/LocaleComparisonTest.php
+++ tests/LocaleComparisonTest.php
@@ -34,16 +34,68 @@ class LocaleComparisonTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @param $locale
+     * @param $locale_in
+     * @dataProvider dataDisplayRegion
+     */
+    public function testGetDisplayRegion($locale, $locale_in)
+    {
+        $localLanguage = Locale::getDisplayRegion($locale, $locale_in);
+
+        $intlLanguage = \Locale::getDisplayRegion($locale, $locale_in);
+
+        $this->assertEquals($intlLanguage, $localLanguage, "region {$locale} with {$locale_in}");
+    }
+
+    public function dataDisplayRegion()
+    {
+        $locales = array();
+
+        foreach ($this->getLocaleList() as $locale) {
+            foreach ($this->getLocaleList() as $innerLocale) {
+                $locales[] = array($locale, $innerLocale);
+            }
+        }
+
+        return $locales;
+    }
+
+    /**
      * @return array
      */
     public function dataListOfLocales()
     {
         $locales = array();
 
-        foreach (\ResourceBundle::getLocales('') as $locale) {
+        foreach ($this->getLocaleList() as $locale) {
             $locales[] = array($locale);
         }
 
         return $locales;
+    }
+
+    private function getLocaleList()
+    {
+        $locales = \ResourceBundle::getLocales('');
+
+        $ignoreList = array(
+            'ar_001',
+            'en_001',
+            'en_150',
+            'es_419',
+            'yi_001',
+        );
+
+        $returnArray = array();
+
+        foreach ($locales as $locale) {
+            if (in_array($locale, $ignoreList)) {
+                continue;
+            }
+
+            $returnArray[] = $locale;
+        }
+
+        return $returnArray;
     }
 }
